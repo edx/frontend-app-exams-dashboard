0a10201a438ed2e0c691c13ccd6a09336afb9fb2
Zach Hancock <zhancock@edx.org>
2023-06-01 16:41:21 -0400

    style: fix name mismatch

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -6,7 +6,7 @@ import { useLocation } from 'react-router-dom';
 
 import ExamsPage from './pages/ExamsPage';
 
-const ExamsDashboardPage = ({ courseId }) => {
+const Dashboard = ({ courseId }) => {
   const { pathname } = useLocation();
   // show or hide header/footer depending on if this app is embedded in instructor dashboard or
   // standalone. If no need for a standalone version, this feature can be removed.
@@ -19,8 +19,8 @@ const ExamsDashboardPage = ({ courseId }) => {
   );
 };
 
-ExamsDashboardPage.propTypes = {
+Dashboard.propTypes = {
   courseId: PropTypes.string.isRequired,
 };
 
-export default ExamsDashboardPage;
+export default Dashboard;
