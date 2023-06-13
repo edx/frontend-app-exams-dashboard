59ede1bc9f8a13e8c762daeaab1bed94f52473da
ilee2u <ilee@2u.com>
2023-06-13 15:26:30 -0400

    feat: initial commit, table outline

---
+++ src/pages/ExamsPage/components/ExamAttemptDataTable.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { DataTable } from '@edx/paragon';
+
+const ExamAttemptDataTable = ({params}) => {
+    <div>
+        <DataTable>
+            
+        </DataTable>
+    </div>
+}
+
+export default ExamAttemptDataTable;
