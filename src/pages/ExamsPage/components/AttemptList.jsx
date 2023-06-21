7ba45aaeb4aac853238b97449406e23211a50254
ilee2u <ilee@2u.com>
2023-06-21 16:42:29 -0400

    feat: POC for data formatting in jsx file
    - Doing i18n formatting seems impossible to do in selectors.js,
    so I am moving the formatting to AttemptsList.
    - This is to be finished later.

--- src/pages/ExamsPage/components/AttemptList.jsx
+++ src/pages/ExamsPage/components/AttemptList.jsx
@@ -1,76 +1,85 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, DataTable } from '@edx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
 const ResetAction = (props) => (
   <Button onClick={() => console.log('Reset', props)}>
     Reset
   </Button>
 );
 
-const AttemptList = ({ attempts }) => (
-  <div>
-    <p>Data Table</p>
-    <DataTable
-      isPaginated
-      isSelectable
-      initialState={{
-        pageSize: 20,
-      }}
-      isSortable
-      itemCount={attempts.length}
-      bulkActions={[
-        <ResetAction />,
-      ]}
-      additionalColumns={[
-        {
-          id: 'action',
-          Header: 'Action',
-          // TODO: For MST-1945, make this button actually "reset" (i.e. delete)
-          // the exam attempt for the cell's respective row after confirmation.
-          Cell: ({ row }) => <Button variant="link" size="sm" onClick={() => console.log(`Resetting ${row.values.name}`)}>Reset</Button>,
-        }
-      ]}
-      data={attempts}
-              // TODO after laundry: use i18n package's "formatMessage", "formattedDate" and all that
+const AttemptList = ({ attempts }) => {
+  const { formatMessage, formatDate } = useIntl();
 
-      columns={[
-        {
-          Header: 'Exam Name',
-          accessor: 'exam_name',
-        },
-        {
-          Header: 'Username',
-          accessor: 'username',
-        },
-        {
-          Header: 'Time Limit',
-          accessor: 'time_limit',
-        },
-        {
-          Header: 'Exam Type',
-          accessor: 'exam_type',
-        },
-        {
-          Header: 'Started At',
-          accessor: 'started_at',
-        },
-        {
-          Header: 'Completed At',
-          accessor: 'completed_at',
-        },
-        {
-          Header: 'Status',
-          accessor: 'status',
-        },
-      ]}
-    >
-      <DataTable.TableControlBar />
-      <DataTable.Table />
-      <DataTable.EmptyTable content="No results found" />
-      <DataTable.TableFooter />
-    </DataTable>
-  </div>
-);
+  return (
+    <div>
+      <p>Data Table</p>
+      <DataTable
+        isPaginated
+        isSelectable
+        initialState={{
+          pageSize: 20,
+        }}
+        isSortable
+        itemCount={attempts.length}
+        bulkActions={[
+          <ResetAction />,
+        ]}
+        additionalColumns={[
+          {
+            id: 'action',
+            Header: 'Action',
+            // TODO: For MST-1945, make this button actually "reset" (i.e. delete)
+            // the exam attempt for the cell's respective row after confirmation.
+            Cell: ({ row }) => <Button variant="link" size="sm" onClick={() => console.log(`Resetting ${row.values.name}`)}>Reset</Button>,
+          }
+        ]}
+        data={attempts}
+        columns={[
+          {
+            Header: 'Exam Name',
+            // TODO: Do this for the rest of these.
+            Cell: ({ row }) => (formatMessage({
+              id: 'AttemptsList.exam_name',
+              defaultMessage: row.original.exam_name,
+              description: 'Name of the exam attempted',
+            })),
+          },
+          {
+            Header: 'Username',
+            accessor: 'username',
+          },
+          {
+            Header: 'Time Limit',
+            accessor: 'time_limit',
+          },
+          {
+            Header: 'Exam Type',
+            accessor: 'exam_type',
+          },
+          {
+            Header: 'Started At',
+            accessor: 'started_at',
+          },
+          {
+            Header: 'Completed At',
+            accessor: 'completed_at',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+        ]}
+      >
+        <DataTable.TableControlBar />
+        <DataTable.Table />
+        <DataTable.EmptyTable content="No results found" />
+        <DataTable.TableFooter />
+      </DataTable>
+    </div>
+  );
+};
 
 AttemptList.propTypes = {
   attempts: PropTypes.arrayOf(PropTypes.object).isRequired, // eslint-disable-line react/forbid-prop-types
