088a94be69261fc219949c0d8563a7c24a6ab65a
ilee2u <ilee@2u.com>
2023-06-22 16:28:58 -0400

    test: AttemptList test completed

--- src/pages/ExamsPage/components/AttemptList.test.jsx
+++ src/pages/ExamsPage/components/AttemptList.test.jsx
@@ -6,16 +6,46 @@ import AttemptList from './AttemptList';
 jest.unmock('react');
 
 describe('AttemptList', () => {
-  const defaultAttemptsData = [{
-    exam_name: 'Exam 1',
-    username: 'username',
-    time_limit: 60,
-    exam_type: 'Timed',
-    started_at: '2023-04-05T19:27:16.000000Z',
-    completed_at: '2023-04-05T19:27:17.000000Z',
-    status: 'completed',
-  }];
+  const defaultAttemptsData = [
+    {
+      exam_name: 'Exam 1',
+      username: 'username',
+      time_limit: 60,
+      exam_type: 'Timed',
+      started_at: '2023-04-05T19:27:16.000000Z',
+      completed_at: '2023-04-05T19:27:17.000000Z',
+      status: 'completed',
+    },
+    {
+      exam_name: 'Exam 2',
+      username: 'username',
+      time_limit: 60,
+      exam_type: 'Proctored',
+      started_at: '2023-04-05T19:37:16.000000Z',
+      completed_at: '2023-04-05T19:37:17.000000Z',
+      status: 'completed',
+    },
+  ];
   it('snapshot', () => {
     expect(render(<AttemptList attempts={defaultAttemptsData} />)).toMatchSnapshot();
+  });
+  // table test
+    // for each item in defaultAttemptsData
+      // make sure there exists a row where all the data fields exist in separate td's, possibly in sequence if we can do that via testing lib
+      // This method from: https://github.com/testing-library/dom-testing-library/issues/583#issuecomment-992999990
+  it('data table', () => {
+    render(<AttemptList attempts={defaultAttemptsData} />);
+    defaultAttemptsData.forEach((attempt, index) => {
+      // Expect a row to be in the table for each attempt in the data
+      expect(screen.getAllByRole('row', {
+        exam_name: attempt.exam_name,
+        username: attempt.username,
+        time_limit: attempt.time_limit,
+        exam_type: attempt.exam_type,
+        started_at: attempt.started_at,
+        completed_at: attempt.completed_at,
+        status: attempt.status,
+      })[index]).toBeInTheDocument();
+    })
   });
 });
