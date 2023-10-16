6980ad6564748ace00f2c4d16ed446c3a5fd564c
Zach Hancock <zhancock@edx.org>
2023-10-16 13:46:49 -0400

    test: modal behavior by status

--- src/pages/ExamsPage/components/ReviewExamAttemptButton.test.jsx
+++ src/pages/ExamsPage/components/ReviewExamAttemptButton.test.jsx
@@ -14,12 +14,12 @@ const mockMakeNetworkRequest = jest.fn();
 // nomally mocked for unit tests but required for rendering/snapshots
 jest.unmock('react');
 
-const reviewButton = (
+const reviewButton = (status = constants.ExamAttemptStatus.second_review_required) => (
   <ReviewExamAttemptButton
     username="username"
     examName="examName"
     attemptId={0}
-    attemptStatus={constants.ExamAttemptStatus.second_review_required}
+    attemptStatus={status}
     severity={1.0}
     submissionReason="Submitted by user"
   />
@@ -31,15 +31,15 @@ describe('ReviewExamAttemptButton', () => {
     hooks.useModifyExamAttempt.mockReturnValue(mockMakeNetworkRequest);
   });
   it('Test that the ReviewExamAttemptButton matches snapshot', () => {
-    expect(render(reviewButton)).toMatchSnapshot();
+    expect(render(reviewButton())).toMatchSnapshot();
   });
   it('Modal appears upon clicking button', () => {
-    render(reviewButton);
+    render(reviewButton());
     screen.getByText('Review Required').click();
     expect(screen.getByText('Please confirm your choice.')).toBeInTheDocument();
   });
   it('Clicking the No button closes the modal', () => {
-    render(reviewButton);
+    render(reviewButton());
     screen.getByText('Review Required').click();
     screen.getByText('No (Cancel)').click();
     // Using queryByText here allows the function to throw
@@ -48,7 +48,7 @@ describe('ReviewExamAttemptButton', () => {
   it('Clicking the Verify button calls the modify exam attempt hook', () => {
     const mockModifyExamAttempt = jest.fn();
     jest.spyOn(hooks, 'useModifyExamAttempt').mockImplementation(() => mockModifyExamAttempt);
-    render(reviewButton);
+    render(reviewButton());
     screen.getByText('Review Required').click();
     screen.getByText('Verify').click();
     expect(mockModifyExamAttempt).toHaveBeenCalledWith(0, constants.ExamAttemptActions.verify);
@@ -56,9 +56,40 @@ describe('ReviewExamAttemptButton', () => {
   it('Clicking the Reject button calls the modify exam attempt hook', () => {
     const mockModifyExamAttempt = jest.fn();
     jest.spyOn(hooks, 'useModifyExamAttempt').mockImplementation(() => mockModifyExamAttempt);
-    render(reviewButton);
+    render(reviewButton());
     screen.getByText('Review Required').click();
     screen.getByText('Reject').click();
     expect(mockModifyExamAttempt).toHaveBeenCalledWith(0, constants.ExamAttemptActions.reject);
+  });
+  it('Does not show the modal if the attempt is not reviewable', () => {
+    render(
+      <ReviewExamAttemptButton
+        username="username"
+        examName="examName"
+        attemptId={0}
+        attemptStatus={constants.ExamAttemptStatus.submitted}
+        severity={1.0}
+        submissionReason="Submitted by user"
+      />,
+    );
+    expect(screen.queryByText('Review Required')).not.toBeInTheDocument();
+    expect(screen.queryByText('Manage Review')).not.toBeInTheDocument();
+  });
+  describe('Shows the correct text based on the attempt status', () => {
+    test('review required', () => {
+      render(reviewButton());
+      screen.getByText('Review Required').click();
+      expect(screen.getByText(/attempt requires manual review/i)).toBeInTheDocument();
+    });
+    test('existing review', () => {
+      render(reviewButton(constants.ExamAttemptStatus.verified));
+      screen.getByText('Manage Review').click();
+      expect(screen.getByText(/attempt is verified/i)).toBeInTheDocument();
+    });
+    test('error status', () => {
+      render(reviewButton(constants.ExamAttemptStatus.error));
+      screen.getByText('Review Required').click();
+      expect(screen.getByText(/attempt has been terminated due to an error/i)).toBeInTheDocument();
+    });
   });
 });
